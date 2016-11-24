6b3830aac3025475e1620f00af1bb33cd39dda39
Alex Robinson <arobinson@zendesk.com>
2016-11-25 01:15:37 +1100

    feat: make app a functional component

--- app/shared/pages/app/app.js
+++ app/shared/pages/app/app.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import { Match, Link } from 'react-router';
 import TodosPage from './../todos-page';
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <h1>Alex's React Starter Pack</h1>
-        <Link to='/home'>Home</Link>
-        <Link to='/todos'>Todos</Link>
-        <Match pattern="/todos" component={TodosPage}/>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <h1>Alexs React Starter Pack</h1>
+    <Link to='/home'>Home</Link>
+    <Link to='/todos'>Todos</Link>
+    <Match pattern="/todos" component={TodosPage}/>
+  </div>
+);
 
 export default App;
