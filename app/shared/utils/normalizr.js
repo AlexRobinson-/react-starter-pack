04899a8138c6636608f528a8613adff53db3b13c
Alex Robinson <arobinson@zendesk.com>
2016-11-26 01:27:39 +1100

    fix: small cleanup

--- app/shared/utils/normalizr.js
+++ app/shared/utils/normalizr.js
@@ -1,9 +1,7 @@
 import { normalize, arrayOf } from 'normalizr';
 import { dataByKey } from './../data-types';
 
-export const normalizeResponse = (dataType, response) => {
-  return normalize(
-    { [dataType]: Array.isArray(response) ? response : [response] },
-    { [dataType]: arrayOf(dataByKey[dataType].schema) }
-  );
-};
+export const normalizeResponse = (dataType, response) => normalize(
+  { [dataType]: Array.isArray(response) ? response : [response] },
+  { [dataType]: arrayOf(dataByKey[dataType].schema) }
+);
