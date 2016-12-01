b1830c5e440b701ee5e2cdc8299d5be552eba9b4
Alex Robinson <arobinson@zendesk.com>
2016-12-02 00:00:53 +1100

    feat: moved todo form to its own component

---
+++ app/shared/modules/todo/components/todo-form/todo-form.js
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react';
+
+class TodoForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: { ...props.todo }
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    if (props.todo.id !== this.state.todo.id) {
+      this.setState({
+        todo: { ...props.todo }
+      });
+    }
+  }
+
+  createTodo() {
+    if (!this.state.todo.title) {
+      return '';
+    }
+
+    this.props.onSubmit({ ...this.state.todo });
+  }
+
+  updateTodo(update) {
+    this.setState({
+      todo: {
+        ...this.state.todo,
+        ...update
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.todo.title || ''}
+          onChange={event => this.updateTodo({title: event.target.value})}
+        />
+        <button onClick={() => this.createTodo()}>{this.props.submitText}</button>
+      </div>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  todo: PropTypes.shape({
+    title: PropTypes.string
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  submitText: PropTypes.string
+};
+
+TodoForm.defaultProps = {
+  submitText: 'Submit'
+};
+
+
+export default TodoForm;
+
--- app/shared/modules/todo/containers/add-todo/add-todo.js
+++ app/shared/modules/todo/containers/add-todo/add-todo.js
@@ -3,38 +3,36 @@ import { connect } from 'react-redux';
 import { v4 } from 'uuid';
 import { TODO_TYPE } from './../../../../constants/data-types';
 import { addData } from './../../../../actions/item-actions';
+import TodoForm from './../../components/todo-form';
 
 class AddTodo extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      inputTitle: ''
+      todo: {}
     }
   }
 
-  createTodo () {
+  createTodo(todo) {
     this.props.addData(
       TODO_TYPE,
       {
-        id: v4(),
-        title: this.state.inputTitle
+        ...todo,
+        id: v4()
       }
     );
-    this.setState({
-      inputTitle: ''
-    });
   }
 
-  render () {
+  render() {
     return (
       <div>
         Add Todo
-        <input
-          value={this.state.inputTitle}
-          onChange={event => this.setState({inputTitle: event.target.value})}
+        <TodoForm
+          todo={{}}
+          onSubmit={todo => this.createTodo(todo)}
+          submitText='Add Todo'
         />
-        <button onClick={() => this.createTodo()}>Add Todo</button>
       </div>
     )
   }
